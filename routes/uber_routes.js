1463d4d08545f9742ef39eb900cf6711869785c3
Emanuel <emanuel@projectcodex.co>
2015-08-26 17:53:08 +0200

    Changing backend to only access database once

--- routes/uber_routes.js
+++ routes/uber_routes.js
@@ -1,8 +1,17 @@
+driver_details = {}
+
 exports.get_issues = function (req, res, next) {
 	req.getConnection(function(err, connection){
-		if (err){
+		if (err)
 			return next(err);
-		}
+
+		var input = JSON.parse(JSON.stringify(req.body));
+
+		driver_details.name = input.name,
+		driver_details.surname = input.surname,
+		driver_details.cell_number = input.cell_number,
+		driver_details.email = input.email
+
 		connection.query('SELECT issue, id from issues_table', [], function(err, results) {
         	if (err){
         		return next(err);
@@ -11,6 +20,23 @@ exports.get_issues = function (req, res, next) {
     		res.render( 'step2_issues', {
     			issues : results
     		});
-      });
+		});
 	});
 };
+
+exports.insert_driver_details = function(req, res, next){
+	req.getConnection(function(err, connection){
+		if (err) {
+			return next(err);
+		};
+
+		var input = JSON.parse(JSON.stringify(req.body));
+
+		connection.query("INSERT INTO driver_details (name, surname, cell_number, email) SELECT * FROM (SELECT ?, ?, ?, ?) AS tmp WHERE NOT EXISTS (SELECT name,surname,cell_number,email FROM driver_details WHERE cell_number = ? AND email = ?) LIMIT 1", [input.name, input.surname, input.cell_number, input.email, input.cell_number, input.email], function(err, status){
+			if (err) {
+				console.log(err, status)
+				return next(err);
+			};
+		})
+	})
+}
