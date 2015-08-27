122b8ff58ddac2d03ebd55f4bd19d2c5be5681d4
oyama <oyama@projectcodex.co>
2015-08-27 14:19:14 +0200

    updated -good state

--- public/js/issues_log.js
+++ public/js/issues_log.js
@@ -3,20 +3,8 @@
   (c) 2013 @ElmahdiMahmoud 
   license: http://www.opensource.org/licenses/mit-license.php
 */   
-$(document).delegate("ul", "click", function() {
-   window.location = $(this).find("a").attr("href");
-});
 
-$('input[name="radio-btn"]').wrap('<div class="radio-btn"><i></i></div>');
-$(".radio-btn").on('click', function () {
-    var _this = $(this),
-        block = _this.parent().parent();
-    block.find('input:radio').attr('checked', false);
-    block.find(".radio-btn").removeClass('checkedRadio');
-    _this.addClass('checkedRadio');
-    _this.find('input:radio').attr('checked', true);
-});
-$('input[name="check-box"]').wrap('<div class="check-box"><i></i></div>');
+$('input[name="issues"]').wrap('<div class="check-box"><i></i></div>');
 $.fn.toggleCheckbox = function () {
     this.attr('checked', !this.attr('checked'));
 }
