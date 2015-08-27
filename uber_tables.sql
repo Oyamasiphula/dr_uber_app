6a387e285d680cd53a7b146e1b02ca461b4fee78
oyama <oyama@projectcodex.co>
2015-08-27 11:49:24 +0200

    updated -good state

--- uber_tables.sql
+++ uber_tables.sql
@@ -1,27 +1,15 @@
-use uber_data;
-
-DROP table if exists ref_table;
-DROP table if exists issues_table;
-DROP table if exists driver_details;
-
 create table driver_details (
 							id int not null primary key auto_increment,
 							name varchar(35),
 							surname varchar(35),
-							cell_number varchar(15),
+							cell_number int,
 							email varchar(35)
 							);
 
-create table issues_table(
-						id int not null primary key auto_increment,
-						issue text
-						);
 
 create table ref_table(
-						ref_no int not null primary key auto_increment,
-						driver_details_id int not null,
-						issue_id int not null,
-						date date,
-						foreign key(issue_id) references issues_table(id),
-						foreign key(driver_details_id) references driver_details(id)
-						);
+ref_no int not null primary key auto_increment,
+driver_details_id int not null foreign key(driver_details.id),
+issue_id int not null foreign key(issue_table.id),
+date date
+);
