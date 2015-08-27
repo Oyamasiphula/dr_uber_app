deb8963d354c4476a7f5e7fbc0b005231e60ecd9
blaqsheep <odwa@projectcodex.co>
2015-08-27 11:24:31 +0200

    Issues table insert swcript done

---
+++ issue_insert.sql
@@ -0,0 +1,9 @@
+insert into issues_table (issue) values("Issue with app");
+insert into issues_table (issue)values("Issue with payment");
+insert into issues_table (issue)values("Issue with uploading docs");
+insert into issues_table (issue)values("Add or change vehicle");
+insert into issues_table (issue)values("Add or change partner");
+insert into issues_table (issue)values("Add or change driver"); 
+insert into issues_table (issue)values("How to sign up");
+insert into issues_table (issue)values("Returnig lost and found items");
+insert into issues_table (issue)values("Ratings for the driver issues");
