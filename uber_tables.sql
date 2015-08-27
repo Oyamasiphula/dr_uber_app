47fc5fb6a8ae9ad597dcb27bb0391969b173a470
Emanuel <emanuel@projectcodex.co>
2015-08-27 12:07:19 +0200

    Fixed uber_tables.sql file

--- uber_tables.sql
+++ uber_tables.sql
@@ -1,3 +1,13 @@
+use uber_data;
+
+DROP USER IF EXISTS uber;
+CREATE USER uber IDENTIFIED BY 'Uber_Uber123';
+GRANT ALL ON uber_data.* TO uber;
+
+DROP TABLE IF EXISTS ref_table;
+DROP TABLE IF EXISTS issue_table;
+DROP TABLE IF EXISTS driver_details;
+
 create table driver_details (
 							id int not null primary key auto_increment,
 							name varchar(35),
@@ -8,17 +18,9 @@ create table driver_details (
 
 
 create table ref_table(
-<<<<<<< HEAD
-ref_no int not null primary key auto_increment,
-driver_details_id int not null foreign key(driver_details.id),
-issue_id int not null foreign key(issue_table.id),
-date date
-);
-=======
-						ref_no int not null primary key auto_increment,
-						driver_details_id int not null,
-						issues varchar(20) not null,
-						date date,
-						foreign key(driver_details_id) references driver_details(id)
-						);
->>>>>>> fd4e96827446a47fb65c82d567cd46b4e0c66851
+					ref_no int not null primary key auto_increment,
+					issues varchar(20),
+					driver_details_id int not null,
+					date date,
+					foreign key(driver_details_id) references driver_details(id)
+					);
